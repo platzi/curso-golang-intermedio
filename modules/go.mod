ca014bfaa8a2e971817b418890f8ba9afb559038
Nestor Escoto <nestor@smartifice.com>
2021-05-11 15:50:02 -0600

    Go Modules

---
+++ modules/go.mod
@@ -0,0 +1,8 @@
+module github.com/nestor94/test
+
+go 1.16
+
+require (
+	github.com/donvito/hellomod v1.0.1
+	github.com/donvito/hellomod/v2 v2.0.0 // indirect
+)
---
+++ modules/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"github.com/donvito/hellomod"
+	hellomod2 "github.com/donvito/hellomod/v2"
+)
+
+func main() {
+	hellomod.SayHello()
+	hellomod2.SayHello("Platzi")
+}
