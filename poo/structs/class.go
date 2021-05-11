5fbdbf83d38fbddcabce3e7e68d17ec6d847eb31
Nestor Escoto <nestor@smartifice.com>
2021-05-11 12:58:55 -0600

    Metodos y Funciones

--- poo/structs/class.go
+++ poo/structs/class.go
@@ -7,10 +7,31 @@ type Employee struct {
 	name string
 }
 
+func (e *Employee) SetId(id int) {
+	e.id = id
+}
+
+func (e *Employee) SetName(name string) {
+	e.name = name
+}
+
+func (e *Employee) GetId() int {
+	return e.id
+}
+
+func (e *Employee) GetName() string {
+	return e.name
+}
+
 func main() {
 	e := Employee{}
-	fmt.Printf("%v", e)
+	//fmt.Printf("%v", e)
 	e.id = 1
 	e.name = "Name"
-	fmt.Printf("%v", e)
+	//fmt.Printf("%v", e)
+	e.SetId(5)
+	e.SetName("Name 2")
+	//fmt.Printf("%v", e)
+	fmt.Println(e.GetId())
+	fmt.Println(e.GetName())
 }
