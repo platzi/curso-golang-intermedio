4482a7dc5c0c3212ddfc750b443f031de374b623
Nestor Escoto <nestor@smartifice.com>
2021-05-12 13:18:28 -0600

    Waitgroup

---
+++ concurrency/wg.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func main() {
+
+	var wg sync.WaitGroup
+
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go doSomething(i, &wg)
+	}
+
+	wg.Wait()
+}
+
+func doSomething(i int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	fmt.Printf("Started %d\n", i)
+	time.Sleep(2 * time.Second)
+	fmt.Println("End")
+}
