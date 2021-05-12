d68c4a9702e00c9fdeadc18563586ee1d61f1274
Nestor Escoto <nestor@smartifice.com>
2021-05-12 16:30:16 -0600

    Profiling

--- testing/main.go
+++ testing/main.go
@@ -10,3 +10,10 @@ func GetMax(x, y int) int {
 	}
 	return y
 }
+
+func Fibonacci(n int) int {
+	if n <= 1 {
+		return n
+	}
+	return Fibonacci(n-1) + Fibonacci(n-2)
+}
--- testing/main_test.go
+++ testing/main_test.go
@@ -44,3 +44,20 @@ func TestMax(t *testing.T) {
 		}
 	}
 }
+
+func TestFib(t *testing.T) {
+	tables := []struct {
+		a int
+		n int
+	}{
+		{1, 1},
+		{8, 21},
+		{50, 12586269025},
+	}
+	for _, item := range tables {
+		fib := Fibonacci(item.a)
+		if fib != item.n {
+			t.Errorf("Fibonacci was incorrect, got %d expected %d", fib, item.n)
+		}
+	}
+}
