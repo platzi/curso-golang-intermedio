0b5377bdbbac76a30a9f814eeb26ef37c94cdab7
Nestor Escoto <nestor@smartifice.com>
2021-05-12 15:28:01 -0600

    Definiendo Workers, Jobs y Dispatchers

---
+++ project/main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type Job struct {
+	Name   string
+	Delay  time.Duration
+	Number int
+}
+
+type Worker struct {
+	Id         int
+	JobQueue   chan Job
+	WorkerPool chan chan Job
+	QuitChan   chan bool
+}
+
+type Dispatcher struct {
+	WorkerPool chan chan Job
+	MaxWorkers int
+	JobQueue   chan Job
+}
+
+func NewWorker(id int, workerPool chan chan Job) *Worker {
+	return &Worker{
+		Id:         id,
+		JobQueue:   make(chan Job),
+		WorkerPool: workerPool,
+		QuitChan:   make(chan bool),
+	}
+}
+
+func (w Worker) Start() {
+	go func() {
+		for {
+			w.WorkerPool <- w.JobQueue
+
+			select {
+			case job := <-w.JobQueue:
+				fmt.Printf("Worker with id %d Started\n", w.Id)
+				fib := Fibonacci(job.Number)
+				time.Sleep(job.Delay)
+				fmt.Printf("Worker with id %d Finished with result %d\n", w.Id, fib)
+			case <-w.QuitChan:
+				fmt.Printf("Worker with id %d Stopped\n", w.Id)
+			}
+		}
+	}()
+}
+
+func (w Worker) Stop() {
+	go func() {
+		w.QuitChan <- true
+	}()
+}
+
+func Fibonacci(n int) int {
+	if n <= 1 {
+		return n
+	}
+	return Fibonacci(n-1) + Fibonacci(n-2)
+}
+
+func NewDispatcher(jobQueue chan Job, maxWorkers int) *Dispatcher {
+	worker := make(chan chan Job, maxWorkers)
+	return &Dispatcher{
+		JobQueue:   jobQueue,
+		MaxWorkers: maxWorkers,
+		WorkerPool: worker,
+	}
+}
+
+func (d *Dispatcher) Dispatch() {
+	for {
+		select {
+		case job := <-d.JobQueue:
+			go func() {
+				workerJobQueue := <-d.WorkerPool
+				workerJobQueue <- job
+			}()
+		}
+	}
+}
