93ed88b73722fc72ac85a0412059482e4e53796f
Nestor Escoto <nestor@smartifice.com>
2021-05-12 13:40:04 -0600

    Buffered Channels como Semaforos

---
+++ concurrency/sem.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+// c := [][]
+// c := [goRoutine1][goRoutine2]
+// c:= [goRoutine3][goRoutine2]
+
+func main() {
+	c := make(chan int, 5)
+	var wg sync.WaitGroup
+	for i := 0; i < 10; i++ {
+		c <- 1
+		wg.Add(1)
+		go doSomething(i, &wg, c)
+	}
+	wg.Wait()
+}
+
+func doSomething(i int, wg *sync.WaitGroup, c chan int) {
+	defer wg.Done()
+	fmt.Printf("Id %d started\n", i)
+	time.Sleep(4 * time.Second)
+	fmt.Printf("Id %d finished\n", i)
+	<-c
+}
