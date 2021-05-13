88d27724e1de1cdc30d305b0fc08e48083b92c6e
Nestor Escoto <nestor@smartifice.com>
2021-05-13 14:16:07 -0600

    Abstract Factory

---
+++ factory/main.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+// SMS Email
+
+type INotificationFactory interface {
+	SendNotification()
+	GetSender() ISender
+}
+
+type ISender interface {
+	GetSenderMethod() string
+	GetSenderChannel() string
+}
+
+type SMSNotification struct {
+}
+
+func (SMSNotification) SendNotification() {
+	fmt.Println("Sending Notification via SMS")
+}
+
+func (SMSNotification) GetSender() ISender {
+	return SMSNotificationSender{}
+}
+
+type SMSNotificationSender struct {
+}
+
+func (SMSNotificationSender) GetSenderMethod() string {
+	return "SMS"
+}
+
+func (SMSNotificationSender) GetSenderChannel() string {
+	return "Twilio"
+}
